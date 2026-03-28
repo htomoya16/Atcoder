55075f36f3686e13bed7c903af7cc730bb6298d9
htomoya16 <h.tomotomo.family@gmail.com>
2026-03-28 16:50:22 +0900

    APG4bPython finish

--- practice/APG4bPython/ex24/main.py
+++ practice/APG4bPython/ex24/main.py
@@ -1,8 +1,26 @@
 import sys
+
+sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 
+
+def popcount(n):
+    return n.bit_count()
+
+
+def f(n):
+    if n == 0:
+        return 0
+    if n >= 0 and popcount(n) % 2 == 1:
+        return f(n - 1) + 1
+    if n >= 0 and popcount(n) % 2 == 0:
+        return f(n - 2) + 1
+
+
 def main():
-    pass
+    n = int(input())
+    print(f(n))
+
 
 if __name__ == "__main__":
     main()
